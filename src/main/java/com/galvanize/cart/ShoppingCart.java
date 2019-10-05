f0c314caa8f143bc358bcfb978a4460e4c2f6b19
rlwing <rob@wingjr.com>
2019-10-05 12:04:04 -0700

    Added solution for primary acceptance criteria

--- src/main/java/com/galvanize/cart/ShoppingCart.java
+++ src/main/java/com/galvanize/cart/ShoppingCart.java
@@ -1,4 +1,76 @@
 package com.galvanize.cart;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ShoppingCart {
+
+    private List<LineItem> cart = new ArrayList<>();
+
+//    private LineItem lineItem = null;
+
+    public BigDecimal totalPrice() {
+        BigDecimal total = new BigDecimal("0.00");
+
+        for(LineItem item : cart){
+            total = total.add(item.price.multiply(BigDecimal.valueOf(item.quantity)));
+        }
+
+        return total;
+    }
+
+    public boolean isEmpty() {
+//        return lineItem == null;
+        return cart.isEmpty();
+    }
+
+    public void addItem(int quantity, String name, BigDecimal price, boolean onSale) {
+        cart.add(new LineItem(quantity, name, price, onSale));
+    }
+
+    public void addItem(int quantity, String name, BigDecimal price) {
+//        this.lineItem = new LineItem(quantity, name, price);
+        cart.add(new LineItem(quantity, name, price));
+    }
+
+    public int getQuantity() {
+//        return lineItem.quantity;
+        int quantity = 0;
+        for (LineItem item : cart){
+            quantity += item.quantity;
+        }
+        return quantity;
+    }
+
+    public List<LineItem> itemizedList() {
+//        return Collections.emptyList();
+        return cart;
+    }
+
+    public List<LineItem> onSaleItems() {
+        return cart.stream().filter(s -> s.onSale).collect(Collectors.toCollection(ArrayList:: new));
+    }
+
+    class LineItem {
+        int quantity;
+        String name;
+        BigDecimal price;
+        boolean onSale = false;
+
+        private LineItem(int quantity, String name, BigDecimal price) {
+            this.quantity = quantity;
+            this.name = name;
+            this.price = price;
+        }
+
+        private LineItem(int quantity, String name, BigDecimal price, boolean onSale) {
+            this.quantity = quantity;
+            this.name = name;
+            this.price = price;
+            this.onSale = onSale;
+        }
+    }
+
 }
--- src/test/java/com/galvanize/cart/ShoppingCartTest.java
+++ src/test/java/com/galvanize/cart/ShoppingCartTest.java
@@ -1,27 +1,140 @@
 package com.galvanize.cart;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ShoppingCartTest {
 
+    // Class Under Test
+    private ShoppingCart shoppingCart;
+
+    @Before
+    public void setUp() {
+        shoppingCart = new ShoppingCart();
+    }
+
     /* Given that I visit the site, when I begin shopping, then I expect my
        cart to be empty.*/
+    @Test
+    public void isEmpty_newCart_returnTrue() {
+        assertTrue(shoppingCart.isEmpty());
+    }
 
     /*Given I have an empty cart, when I add an Item, then I expect to see
       totalPrice reflect the sum of all the Items in my cart, times the
       quantities of each item.*/
+    @Test
+    public void getTotalPrice_emptyCart_addOneItem_returnPrice() {
+        // Setup
+        int quantity = 1;
+        String name = "widget";
+        BigDecimal price = new BigDecimal("1.11");
+
+        // Exercise
+        shoppingCart.addItem(quantity, name, price);
+
+        // Assert
+        assertEquals(price, shoppingCart.totalPrice());
+
+        // Teardown
+    }
 
     /*Given I have an empty cart, when I add more than one of an item, then
       I expect itemQuantities() to show the number of items I have added.*/
+    @Test
+    public void itemQuantities_emptyCart_returnQuantity() {
+        // Setup
+        int quantity = 2;
+        String name = "widget";
+        BigDecimal price = new BigDecimal("1.11");
+
+        // Exercise
+        shoppingCart.addItem(quantity, name, price);
+
+        // Assert
+        assertEquals(quantity, shoppingCart.getQuantity());
+    }
 
     /*Given I have an empty cart, when I add items, then I expect itemizedList()
       reflect the items I have added along with their price and quantity.*/
+    @Test
+    public void addItems_emptyCart_returnListOfItems() {
+        // Setup
+        int quantity = 2;
+        String name = "widget";
+        BigDecimal price = new BigDecimal("1.11");
+
+        // Exercise
+        shoppingCart.addItem(quantity, name, price);
+        shoppingCart.addItem(quantity, name, price);
+
+        // Assert - list of 2 items
+        List<ShoppingCart.LineItem> cartList = shoppingCart.itemizedList();
+        assertNotNull(cartList);
+        assertFalse(cartList.isEmpty());
+        assertEquals(2, cartList.size());
+        for(ShoppingCart.LineItem item : cartList){
+            assertEquals(name, item.name);
+            assertEquals(quantity, item.quantity);
+            assertEquals(price, item.price);
+        }
+
+    }
 
     /*Given I have an empty cart, when I add more than one of an item, then
       I expect totalPrice to reflect both the item price and quantity.*/
+    @Test
+    public void totalPrice_emptyCart_twoItems_returnExtTotal() {
+        // Setup
+        int quantity = 2;
+        String name = "widget";
+        BigDecimal price = new BigDecimal("1.11");
+
+        // Exercise
+        shoppingCart.addItem(quantity, name, price);
+        shoppingCart.addItem(quantity, name, price);
+
+        // Assert - list of 2 items
+        assertEquals(new BigDecimal("4.44"), shoppingCart.totalPrice());
+    }
 
     /*Given I have a cart with items that are not on sale, when I add items
       that are on sale, I expect onSaleItems() to include only the items on sale.*/
+    @Test
+    public void onSaleItems_returnOnlyOnSaleItems() {
+        // Setup
+        int quantity = 2;
+        String name = "widget";
+        BigDecimal price = new BigDecimal("1.11");
+
+        // Exercise
+        shoppingCart.addItem(quantity, name, price);
+        shoppingCart.addItem(quantity, name, price);
+        shoppingCart.addItem(quantity, name, price, true);
+        shoppingCart.addItem(quantity, name, price, true);
+
+        // Total items in cart is 4
+        assertEquals(4, shoppingCart.itemizedList().size());
+        // Two items are on sale
+        List<ShoppingCart.LineItem> cartList = shoppingCart.onSaleItems();
+        assertNotNull(cartList);
+        assertFalse(cartList.isEmpty());
+        assertEquals(2, cartList.size());
+        for(ShoppingCart.LineItem item : cartList){
+            assertEquals(name, item.name);
+            assertEquals(quantity, item.quantity);
+            assertEquals(price, item.price);
+            assertTrue(item.onSale);
+        }
+    }
+
+    /* given an empty cart, add item 2323 from Widget Catalog */
+
+    /* given an empty cart, add all items of category primary widgets from Widget Catelog */
 }
