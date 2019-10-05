4ceb89b04cc57ba47f6f9bff4ecacf3dba77d009
rlwing <rob@wingjr.com>
2019-10-05 09:40:39 -0700

    initial commit

---
+++ src/test/java/com/galvanize/cart/ShoppingCartTest.java
@@ -0,0 +1,27 @@
+package com.galvanize.cart;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShoppingCartTest {
+
+    /* Given that I visit the site, when I begin shopping, then I expect my
+       cart to be empty.*/
+
+    /*Given I have an empty cart, when I add an Item, then I expect to see
+      totalPrice reflect the sum of all the Items in my cart, times the
+      quantities of each item.*/
+
+    /*Given I have an empty cart, when I add more than one of an item, then
+      I expect itemQuantities() to show the number of items I have added.*/
+
+    /*Given I have an empty cart, when I add items, then I expect itemizedList()
+      reflect the items I have added along with their price and quantity.*/
+
+    /*Given I have an empty cart, when I add more than one of an item, then
+      I expect totalPrice to reflect both the item price and quantity.*/
+
+    /*Given I have a cart with items that are not on sale, when I add items
+      that are on sale, I expect onSaleItems() to include only the items on sale.*/
+}
